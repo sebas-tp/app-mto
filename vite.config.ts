55d783049da337a91e1539eb0eac736eec3624c2
sebas-tp <ssasgc0109@gmail.com>
2025-12-23 08:46:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Esto inyecta la variable API_KEY de las variables de entorno de Vercel
+    // en el código del cliente bajo el objeto process.env.API_KEY
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
